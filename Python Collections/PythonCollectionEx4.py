1c86b44e249a4feedede1765e536310159085f66
Mitusha arya <aryamitusha@gmail.com>
2019-11-28 11:32:29 +0530

    Create PythonCollectionEx4.py

---
+++ Python Collections/PythonCollectionEx4.py
@@ -0,0 +1,5 @@
+#Change the item "Pink" to "Blue": ["black", "yellow", "pink", "green", "purple", "white", "grey"]
+
+thislist =["black", "yellow", "pink", "green", "purple", "white", "grey"]
+thislist[2] = "blue"
+print(thislist)
