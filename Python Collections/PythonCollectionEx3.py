afe0bd51a2128b3f783cdf9644cdfb83f8cbeda7
Mitusha arya <aryamitusha@gmail.com>
2019-11-28 11:27:53 +0530

    Create PythonCollectionEx3.py

---
+++ Python Collections/PythonCollectionEx3.py
@@ -0,0 +1,3 @@
+#Return the items from the beginning to "green": ["black", "yellow", "pink", "green", "purple", "white", "grey"]
+thislist =["black", "yellow", "pink", "green", "purple", "white", "grey"]
+print(thislist[:4])
