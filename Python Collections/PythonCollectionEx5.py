22f7a22db18cc66428202f92201beba1da01e5f0
Mitusha arya <aryamitusha@gmail.com>
2019-11-28 11:48:47 +0530

    Create PythonCollectionEx5.py

---
+++ Python Collections/PythonCollectionEx5.py
@@ -0,0 +1,5 @@
+#Check if "pink" is present in the list:["black", "yellow", "pink", "green", "purple", "white", "grey"]
+
+thislist = ["black", "yellow", "pink", "green", "purple", "white", "grey"]
+if "pink" in thislist:
+  print("Yes, 'pink' is in the list")
